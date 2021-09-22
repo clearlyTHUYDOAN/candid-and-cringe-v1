acb1530eb618d839be7a0d336728acf7f09f626a
clearlythuydoan <clearlythuydoan@gmail.com>
2021-09-22 15:11:25 -0400

    Update index.js with semantic layout for home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,37 +1,92 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import Layout from "../components/layout"
 import SEO from "../components/seo"
-import Button from "../components/button"
+import "../components/styles.css"
 
 class IndexPage extends React.Component {
   render() {
-    const siteTitle = "Gatsby Starter Personal Website"
-
     return (
-      <Layout location={this.props.location} title={siteTitle}>
-        <SEO
-          title="Home"
-          keywords={[`blog`, `gatsby`, `javascript`, `react`]}
-        />
-        <img style={{ margin: 0 }} src="./GatsbyScene.svg" alt="Gatsby Scene" />
-        <h1>
-          Hey people{" "}
-          <span role="img" aria-label="wave emoji">
-            👋
-          </span>
-        </h1>
-        <p>Welcome to your new Gatsby website. You are on your home page.</p>
-        <p>
-          This starter comes out of the box with styled components and Gatsby's
-          default starter blog running on Netlify CMS.
-        </p>
-        <p>Now go build something great!</p>
-        <Link to="/blog/">
-          <Button marginTop="35px">Go to Blog</Button>
-        </Link>
-      </Layout>
+      <>
+        <SEO title="Home" />
+        <nav>
+          <ul>
+            <li>
+              <Link to="/about/">About</Link>
+            </li>
+            <li>
+              <a href="/episodes/">Episodes</a>
+            </li>
+            <li>
+              <a href="https://www.patreon.com/candidandcringe">Patreon</a>
+            </li>
+            <li>
+              <a href="/subscribe/">Subscribe</a>
+            </li>
+          </ul>
+        </nav>
+        <header class="test">
+          A podcast breaking barriers one candid conversation at a time
+        </header>
+        <main>
+          <h1 class="test">Candid & Cringe</h1>
+          <p>
+            a solo podcast with a conversational tone - a reflective, (sometimes
+            funny) story-telling and thought-provoking experience.
+          </p>
+        </main>
+        <button>More Episodes</button>
+        <section>
+          <h2 class="test">
+            I’m a social media marketer turned developer looking to learn & grow
+            with my community
+          </h2>
+          <p>
+            Tune in to my bi-weekly podcast where I explore topics like mental
+            health, uplifting women of colour, women in tech, early career
+            developers, people pivoting in their careers, folks in the
+            accessibility & disability communities and so much more!
+          </p>
+          <button>About C&C</button>
+          <button>Play Trailer</button>
+        </section>
+        <section>
+          <p>
+            Click here to listen to Candid & Cringe now, for collaborations and
+            inquiries please email me at hi@candidandcringe.com
+          </p>
+          <sub>Also available on Spotify and Apple Podcasts</sub>
+        </section>
+        <section>
+          <p>Subscribe to get new episodes directly to your mailbox</p>
+          <form>
+            <input type="email" aria-label="Email" />
+            <button>Submit</button>
+          </form>
+        </section>
+        <footer>
+          <ul>
+            <li>
+              <a
+                href="mailto:hi@candidandcringe.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Email
+              </a>
+            </li>
+            <li>
+              <a href="https://twitter.com/candidandcringe">Twitter</a>
+            </li>
+            <li>
+              <a href="https://www.instagram.com/candidandcringe/">Instagram</a>
+            </li>
+            <li>
+              <a href="https://www.patreon.com/candidandcringe">Patreon</a>
+            </li>
+          </ul>
+        </footer>
+      </>
     )
   }
 }
