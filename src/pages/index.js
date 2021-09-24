f9c58915e1fe67ce99e3599ff09e7c5538ebbc3f
clearlythuydoan <clearlythuydoan@gmail.com>
2021-09-23 23:13:10 -0400

    Style footer

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,11 @@ import multiEpisodePlayerBackground from "../images/multi-episode-player-backgro
 import playerPlaceholder from "../images/player-placeholder.jpg"
 import sageLeafBackground from "../images/sage-leaf-background.jpg"
 
+import emailIcon from "../images/mail.png"
+import twitterIcon from "../images/twitter.png"
+import instagramIcon from "../images/instagram.png"
+import patreonIcon from "../images/patreon.png"
+
 const DeepJadeLeafBackground = styled.header`
   display: flex;
   flex-direction: column;
@@ -100,6 +105,28 @@ const SubtextContainer = styled.div`
   font-size: 1.2em;
 `
 
+const Footer = styled.footer`
+  display: flex;
+  justify-content: center;
+
+  background-color: #324d4c;
+  padding-top: 50px;
+  padding-bottom: 50px;
+`
+
+const Link = styled.a`
+  box-shadow: none;
+`
+const SocialIcon = styled.img`
+  margin: 0;
+`
+
+const SocialLinksContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 20%;
+`
+
 class IndexPage extends React.Component {
   render() {
     return (
@@ -168,28 +195,30 @@ class IndexPage extends React.Component {
             <CTA>Submit</CTA>
           </form>
         </section>
-        <footer>
-          <ul>
-            <li>
-              <a
-                href="mailto:hi@candidandcringe.com"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Email
-              </a>
-            </li>
-            <li>
-              <a href="https://twitter.com/candidandcringe">Twitter</a>
-            </li>
-            <li>
-              <a href="https://www.instagram.com/candidandcringe/">Instagram</a>
-            </li>
-            <li>
-              <a href="https://www.patreon.com/candidandcringe">Patreon</a>
-            </li>
-          </ul>
-        </footer>
+        <Footer>
+          <SocialLinksContainer>
+            <Link
+              href="mailto:hi@candidandcringe.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <SocialIcon src={emailIcon} alt="Mail icon" height={35} />
+            </Link>
+            <Link href="https://twitter.com/candidandcringe">
+              <SocialIcon src={twitterIcon} alt="Twitter icon" height={35} />
+            </Link>
+            <Link href="https://www.instagram.com/candidandcringe/">
+              <SocialIcon
+                src={instagramIcon}
+                alt="Instagram icon"
+                height={35}
+              />
+            </Link>
+            <Link href="https://www.patreon.com/candidandcringe">
+              <SocialIcon src={patreonIcon} alt="Patreon icon" height={35} />
+            </Link>
+          </SocialLinksContainer>
+        </Footer>
       </>
     )
   }
